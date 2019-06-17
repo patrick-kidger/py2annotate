109c56df71c3e5112d6b4e9822d4deff11331c8f
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2019-06-18 00:53:06 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,65 @@
+***********
+py2annotate
+***********
+An extension to Sphinx `autodoc` to augment sphinx documentation with type annotations, when using Python 2 style type annotations.
+
+The Problem
+===========
+Python 2 doesn't support type annotations. Thus writing Python 2 / Python 3 agnostic code with type annotations requires `putting the type annotations in comments <https://mypy.readthedocs.io/en/latest/python2.html>`__:
+
+.. code-block:: python
+
+    def add(x, y):
+        # type: (int, int) -> int
+        """Adds two numbers."""
+        return x + y
+
+But now when documentation is generated with `sphinx <http://www.sphinx-doc.org/en/master/>`__, the documentation doesn't include type annotations:
+
+.. include:: imgs/without-annotations.png
+
+*Example from the* |signatory|_ *project.*
+
+.. _signatory: https://github.com/patrick-kidger/signatory
+.. |signatory| replace:: *signatory*
+
+The Solution
+============
+
+This extension remedies things so that the Sphinx documentation now looks like:
+
+.. include:: imgs/with-annotations.png
+
+.. role:: python(code)
+    :language: python
+
+(which is the same as what you'd get using Python 3 style type annotations e.g. :python:`def add(x: int, y: int) -> int`)
+
+Installation
+============
+Via pip:
+.. code-block:: bash
+
+    pip install py2annotate
+
+It's also just a single file, so copy-paste the code if you want.
+
+Usage
+=====
+Just add 'py2annotate' to the list of extensions in `conf.py`:
+
+.. code-block:: python
+
+    # conf.py
+    ...
+    extensions = ['sphinx.ext.autodoc', 'py2annotate']
+    ...
+
+(It shouldn't actually matter whether it comes before or after `autodoc`.)
+
+Note that Sphinx itself must be run using Python 3, not Python 2. This is because `py2annotate` uses the Python 3 style
+type annotations internally in order to determine the correct annotations.
+
+Known Issues
+============
+None so far! File a report if you run into anything.
---
+++ setup.py
@@ -0,0 +1,54 @@
+import io
+import os
+import setuptools
+
+project = 'py2annotate'
+version = "1.0.0"
+author = "Patrick Kidger"
+copyright = "2019, {}".format(author)
+author_email = "contact@kidger.site"
+url = "https://github.com/patrick-kidger/py2annotate"
+license = "Apache-2.0"
+python_requires = ">=3.0"
+keywords = "signature"
+classifiers = ["Development Status :: 4 - Beta",
+               "Environment :: Web Environment",
+               "Framework :: Sphinx",
+               "Framework :: Sphinx :: Extension",
+               "Intended Audience :: Developers",
+               "Intended Audience :: Education",
+               "Intended Audience :: End Users/Desktop",
+               "Intended Audience :: Science/Research",
+               "Intended Audience :: System Administrators",
+               "License :: OSI Approved :: Apache Software License",
+               "Natural Language :: English",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python :: 3",
+               "Topic :: Documentation",
+               "Topic :: Documentation :: Sphinx",
+               "Topic :: Internet :: WWW/HTTP :: Site Management",
+               "Topic :: Software Development :: Documentation"]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+description = readme.split('\n', maxsplit=4)[-2]
+
+setuptools.setup(name=project,
+                 version=version,
+                 author=author,
+                 author_email=author_email,
+                 maintainer=author,
+                 maintainer_email=author_email,
+                 description=description,
+                 long_description=readme,
+                 url=url,
+                 license=license,
+                 keywords=keywords,
+                 classifiers=classifiers,
+                 zip_safe=False,
+                 python_requires=python_requires,
+                 packages=[project],
+                 install_requires=['sphinx', 'mypy'])
